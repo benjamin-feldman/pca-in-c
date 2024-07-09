997591b5b918cc8ad5f9a9c44e61e3c5f8e60e02
benjamin-feldman <benjamin.feldman666@gmail.com>
2024-07-09 16:17:36 +0200

    some linalg utils
    power_iteration: iteratively compute the eigenvector associated with the largest eigenvalue of a symmetric matrix

--- src/pca.c
+++ src/pca.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <assert.h>
 
 // macro that will print a header containing the name of the matrix
 // uses the # operator of the C preprocessor
-#define PRINT_MATRIX(mat)                                                      \
-  printf("Matrix %s:\n", #mat);                                                \
-  print_matrix(mat)
+#define PRINT_MATRIX(mat) printf("Matrix %s:\n", #mat); print_matrix(mat)
+#define TOL 1e-10 // tolerance for num algos
+#define MAX_ITER 100
 
 // basic 2-D Matrix struct
 typedef struct {
@@ -23,7 +26,7 @@ typedef struct {
 void print_matrix(const Matrix *mat);
 Matrix *matmul(const Matrix *A, const Matrix *B);
 Matrix *malloc_mat(int rows, int cols);
-void free_mat_from_memory(Matrix *mat);
+void free_matrix(Matrix *mat);
 Matrix *identity(int n);
 Matrix *diag(int n, double value);
 LUDecomposition *LUdecompose(const Matrix *mat);
@@ -31,7 +34,7 @@ LUDecomposition *LUdecompose(const Matrix *mat);
 void print_matrix(const Matrix *mat) {
   for (int i = 0; i < mat->rows; i++) {
     for (int j = 0; j < mat->cols; j++) {
-      printf("%8.3f ", mat->data[i][j]);
+      printf("%8.2f ", mat->data[i][j]);
     }
     printf("\n");
   }
@@ -48,7 +51,7 @@ Matrix *malloc_mat(int rows, int cols) {
   return mat;
 }
 
-void free_mat_from_memory(Matrix *mat) {
+void free_matrix(Matrix *mat) {
   for (int i = 0; i < mat->rows; i++) {
     free(mat->data[i]);
   }
@@ -141,7 +144,7 @@ Matrix *solve_lower_triangular(const Matrix *L, const Matrix *b) {
     }
     if (L->data[i][i] == 0) {
       printf("Error: The system is not solvable since the matrix is singular.");
-      free_mat_from_memory(x);
+      free_matrix(x);
       return NULL;
     }
     xi /= L->data[i][i];
@@ -168,7 +171,7 @@ Matrix *solve_upper_triangular(const Matrix *U, const Matrix *b) {
     }
     if (U->data[i][i] == 0) {
       printf("Error: The system is not solvable since the matrix is singular.");
-      free_mat_from_memory(x);
+      free_matrix(x);
       return NULL;
     }
     xi /= U->data[i][i];
@@ -209,8 +212,8 @@ LUDecomposition *LUdecompose(const Matrix *mat) {
         }
         if (U->data[i][i] == 0) {
           printf("Error: LU decomposition failed. Matrix may be singular.\n");
-          free_mat_from_memory(L);
-          free_mat_from_memory(U);
+          free_matrix(L);
+          free_matrix(U);
           return NULL;
         }
         L->data[k][i] = (mat->data[k][i] - sum) / U->data[i][i];
@@ -221,6 +224,7 @@ LUDecomposition *LUdecompose(const Matrix *mat) {
   LUDecomposition *result = malloc(sizeof(LUDecomposition));
   result->L = L;
   result->U = U;
+
   return result;
 }
 
@@ -233,18 +237,18 @@ Matrix *solve_linear_system(Matrix *A, Matrix *b) {
 
   Matrix *y = solve_lower_triangular(LU->L, b);
   if (y == NULL) {
-    free_mat_from_memory(LU->L);
-    free_mat_from_memory(LU->U);
+    free_matrix(LU->L);
+    free_matrix(LU->U);
     free(LU);
     return NULL;
   }
 
   Matrix *x = solve_upper_triangular(LU->U, y);
 
-  free_mat_from_memory(LU->L);
-  free_mat_from_memory(LU->U);
+  free_matrix(LU->L);
+  free_matrix(LU->U);
   free(LU);
-  free_mat_from_memory(y);
+  free_matrix(y);
 
   return x;
 }
@@ -268,36 +272,133 @@ Matrix *invertmat(Matrix *mat){
     for (int k = 0; k < n; k++){
       result->data[k][i] = x->data[k][0]; 
     }
-    free_mat_from_memory(x);
-    free_mat_from_memory(e);
+    free_matrix(x);
+    free_matrix(e);
   }
   return result;
 }
 
+double randfrom(double min, double max){
+  double range = (max - min);
+  double div = RAND_MAX / range;
+  return min + (rand() / div);
+}
+
+Matrix *random_vector(int rows){
+  assert(rows > 0);
+
+  Matrix *v = malloc_mat(rows, 1);
+
+  if (v == NULL){
+    fprintf(stderr, "Error: Memory allocation failed for random vector.\n");
+    return NULL;
+  }
+
+  for (int i = 0; i < rows; i++){
+    v->data[i][0] = randfrom(-1, 1);
+  }
+  return v;
+}
+
+int allclose(const Matrix *v1, const Matrix *v2, double tol){
+  // assert inputs are column vectors of same dimension
+
+  assert(v1->rows == v2->rows && v1->cols == 1 && v2->cols == 1);
+
+  int result = 1;
+  for (int i = 0; i < v1->rows; i++)
+    result = result && (fabs(v1->data[i][0] - v2->data[i][0]) <= tol);
+  return result;
+}
+
+double dotproduct(const Matrix *v1, const Matrix *v2){
+  // assert inputs are column vectors of same dimension
+  assert(v1->rows == v2->rows && v1->cols == 1 && v2->cols == 1);
+
+  double res = 0;
+  for (int i = 0; i < v1->rows; i++)
+    res += v1->data[i][0] * v2->data[i][0];
+  return res;
+}
+
+double norm2(const Matrix *v){
+  // assert input is a column vector
+  assert(v->cols == 1);
+  return sqrt(dotproduct(v, v));
+}
+
+Matrix *scalarmul(const Matrix *mat, double scalar){
+  Matrix *result = malloc_mat(mat->rows, mat->cols);
+  if (result == NULL){
+    fprintf(stderr, "Error: Memory allocation failed.");
+    return NULL;
+  }
+
+  result->rows = mat->rows;
+  result->cols = mat->cols;
+
+  for (int i = 0; i < mat->rows; i++)
+    for (int j = 0; j < mat->cols; j++)
+      result->data[i][j] = scalar * mat->data[i][j];
+  
+  return result;
+}
+
+void copy_data(Matrix *source, Matrix *dest){
+  assert(source->rows == dest->rows && source->cols == dest->cols);
+
+  for (int i = 0; i < source->rows; i++)
+    for (int j = 0; j < source->cols; j++)
+      dest->data[i][j] = source->data[i][j];
+}
+
+Matrix *power_iteration(const Matrix *A, double tol, int max_iter){
+  assert(A->rows == A->cols);
+  assert(tol > 0 && max_iter > 0);
+
+  Matrix *v = random_vector(A->cols);
+
+  v = scalarmul(v, 1.0 / norm2(v));
+  
+  Matrix *previous = zeros(A->cols, 1);
+
+  int iter = 0;
+
+  while (iter < max_iter){
+    copy_data(v, previous);
+    Matrix *temp = matmul(A, v);
+    free_matrix(v);
+    v = scalarmul(temp, 1.0 / norm2(temp));
+    free_matrix(temp);
+    if (allclose(v, previous, TOL))
+      break;
+    ++iter;
+  }
+
+  free_matrix(previous);
+
+  return v;
+}
+
 int main() {
-  Matrix *A, *B, *b;
+  srand(time(NULL)); // set seed for rand
 
-  A = diag(3, -1);
-  B = diag(3, 2);
-  b = ones(3, 1);
+  Matrix *sym_matrix = malloc_mat(3, 3);
 
-  Matrix *C = matmul(A, B);
-  Matrix *D = matsum(A, B);
+  sym_matrix->data[0][0] = 2.92;
+  sym_matrix->data[0][1] = 0.86;
+  sym_matrix->data[0][2] = -1.15;
+  sym_matrix->data[1][0] = 0.86;
+  sym_matrix->data[1][1] = 6.51;
+  sym_matrix->data[1][2] = 3.32;
+  sym_matrix->data[2][0] = -1.15;
+  sym_matrix->data[2][1] = 3.32;
+  sym_matrix->data[2][2] = 4.57;
 
-  LUDecomposition *LU = LUdecompose(C);
-  Matrix *L = LU->L;
-  Matrix *U = LU->U;
+  Matrix *eigenvector_1 = power_iteration(sym_matrix, TOL, MAX_ITER);
 
-  Matrix *y = solve_lower_triangular(L, b);
-  Matrix *x = solve_upper_triangular(U, y);
-  Matrix *E = invertmat(B);
+  PRINT_MATRIX(sym_matrix);
+  PRINT_MATRIX(eigenvector_1);
 
-  if (C == NULL || D == NULL)
-    return 1;
-  PRINT_MATRIX(A);
-  PRINT_MATRIX(B);
-  PRINT_MATRIX(C);
-  PRINT_MATRIX(x);
-  PRINT_MATRIX(E);
   return 0;
 }
